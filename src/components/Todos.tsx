1c2053d7d466829de3f074993a0606a8ec29c8a3
kboysm <lishal12@naver.com>
2021-03-09 22:47:10 +0900

    array-callback-return 오류 발생

---
+++ src/components/Todos.tsx
@@ -0,0 +1,11 @@
+import { Row } from './Row'
+import { data } from '../todos'
+export const Todos = () => {
+    return (
+        <section>
+            {data.map((todo) => {
+                <Row key={todo.id} todo={todo}/>
+            })}
+        </section>
+    )
+}
